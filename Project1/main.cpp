649d224efdddf13391292b4f723469828af1e8c1
Aver <kuba.grych@gmail.com>
2024-04-10 00:11:26 +0200

    akjdghjgjahjfg szalenstwo

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,12 +6,16 @@
 #include "./include/DynamicArray.h"
 #include "./include/TableGenerator.h"
 #include <chrono>
+#include <fstream>
 
-std::vector<long> TestDoublyLinkedListTimes(int size) {
+std::vector<double> TestDoublyLinkedListTimes(int size) {
+
     auto data = generateRandomVector(size);
-    DoublyLinkedList<int> doublyLinkedList;
-    std::vector<long> times;
 
+    DoublyLinkedList<int> doublyLinkedList;
+    std::vector<double> times;
+
+    doublyLinkedList.pushBack(1);
     for(int number : data){
         doublyLinkedList.pushBack(number);
     }
@@ -19,106 +23,105 @@ std::vector<long> TestDoublyLinkedListTimes(int size) {
     auto start = std::chrono::high_resolution_clock::now();
     doublyLinkedList.pushBack(data[0]);
     auto end = std::chrono::high_resolution_clock::now();
-    auto duration = start - end;
-    times.push_back(duration.count()*100000);
+    std::chrono::duration<double, std::nano> duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     doublyLinkedList.pushFront(data[0]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     doublyLinkedList.popBack();
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     doublyLinkedList.popFront();
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     doublyLinkedList.insert(size/2,data[0]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     doublyLinkedList.erase(size/2);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     doublyLinkedList.contains(data[size/2]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     return times;
 }
 
-std::vector<long> TestSinglyLinkedListTimes(int size) {
+std::vector<double> TestSinglyLinkedListTimes(int size) {
     auto data = generateRandomVector(size);
     SinglyLinkedList<int> singlyLinkedList;
-    std::vector<long> times;
+    std::vector<double> times;
 
     for(int number : data){
         singlyLinkedList.append(number);
     }
-
     auto start = std::chrono::high_resolution_clock::now();
     singlyLinkedList.append(data[0]);
     auto end = std::chrono::high_resolution_clock::now();
-    auto duration = start - end;
-    times.push_back(duration.count()*100000);
+    std::chrono::duration<double, std::nano> duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     singlyLinkedList.prepend(data[0]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     singlyLinkedList.remove(size-1);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     singlyLinkedList.remove(0);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     singlyLinkedList.insert(size/2,data[0]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     singlyLinkedList.remove(size/2);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     singlyLinkedList.contains(data[size/2]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     return times;
 }
 
-std::vector<long> TestDynamicArrayTimes(int size) {
+std::vector<double> TestDynamicArrayTimes(int size) {
     auto data = generateRandomVector(size);
     DynamicArray<int> dynamicArray;
-    std::vector<long> times;
+    std::vector<double> times;
 
     for(int number : data){
         dynamicArray.append(number);
@@ -127,59 +130,73 @@ std::vector<long> TestDynamicArrayTimes(int size) {
     auto start = std::chrono::high_resolution_clock::now();
     dynamicArray.append(data[0]);
     auto end = std::chrono::high_resolution_clock::now();
-    auto duration = start - end;
-    times.push_back(duration.count()*100000);
+    std::chrono::duration<double, std::nano> duration = end - start;
+    times.push_back(duration.count());
+    std::cout<<times[0]<< std::endl;
 
     start = std::chrono::high_resolution_clock::now();
     dynamicArray.prepend(data[0]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
+    //std::cout<<duration.count() << std::endl;
 
     start = std::chrono::high_resolution_clock::now();
     dynamicArray.remove(size-1);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     dynamicArray.remove(0);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     dynamicArray.insert(size/2,data[0]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     dynamicArray.remove(size/2);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     start = std::chrono::high_resolution_clock::now();
     dynamicArray.contains(data[size/2]);
     end = std::chrono::high_resolution_clock::now();
-    duration = start - end;
-    times.push_back(duration.count()*100000);
+    duration = end - start;
+    times.push_back(duration.count());
 
     return times;
+}
+
+void SaveMeasurementsToFile(std::vector<double> data, std::string filename){
+    std::ofstream outputFile(filename);
+    if (outputFile.is_open()) {
+        for (int i = 0; i < data.size(); i++) {
+            outputFile << data[i] << "\n";
+        }
+        outputFile.close();
+        std::cout << "Measurements saved to " << filename << std::endl;
+    } else {
+        std::cerr << "Unable to open file for writing." << std::endl;
+    }
 }
 
 int main() {
     int ARRAY_SIZES[] = {5000, 10000, 50000, 100000};
     for(int size : ARRAY_SIZES){
-        std::vector<long> DLLTimes = TestDoublyLinkedListTimes(size);
-        std::vector<long> SLLTimes = TestSinglyLinkedListTimes(size);
-        std::vector<long> DATimes = TestDynamicArrayTimes(size);
+        std::vector<double> DLLTimes = TestDoublyLinkedListTimes(size);
+        std::vector<double> SLLTimes = TestSinglyLinkedListTimes(size);
+        std::vector<double> DATimes = TestDynamicArrayTimes(size);
 
-
+        SaveMeasurementsToFile(DLLTimes, std::to_string(size)+"_DoublyLinkedList");
+        SaveMeasurementsToFile(SLLTimes, std::to_string(size)+"_SinglyLinkedList");
+        SaveMeasurementsToFile(DATimes, std::to_string(size)+"_DynamicArray");
     }
-
-
-
     return 0;
 }
